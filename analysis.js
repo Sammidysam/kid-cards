986ae1ef604ee84d270881b8859bbe5cc5ce2d59
Clara Na <csn5aw@virginia.edu>
2018-10-06 12:51:12 -0400

    added medication history analysis function

--- analysis.js
+++ analysis.js
@@ -13,9 +13,25 @@ var analysis = {
 		else
 			return "Thank you! No information about last fever."
 	},
+	medhist: function (patient, medication) {
+		var started = patient.notes.sort(function (a, b) {
+			return new Date(b.date) - new Date(a.date)
+		}).find(function (e, medication) {
+			return e.body.includes(`started taking ${medication}`)
+		})
+
+		if (started !== undefined)
+			return `Thank you! You started taking ${medication} on ${moment(started.date).fromNow()} with notes: ${started.body}`
+		else
+			return `Thank you! No previous information about ${medication}.`
+	},
 	analyze: function (patient, text) {
 		if (text.includes("fever")) {
 			return this.fever(patient);
+		} if (text.includes("Adderall")) {
+			return this.medhist(patient, "Adderall")
+		} if (text.includes("Focalin")) {
+			return this.medhist(patient, "Focalin")
 		} else {
 			return "Thank you! I cannot provide any guidance right now, but I will record your report for future analysis.";
 		}
