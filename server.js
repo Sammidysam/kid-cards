f2e862720b44cd781d61c434d7c81500348b4b47
Sam Craig <craigsamm@gmail.com>
2018-10-06 09:38:15 -0400

    Store the text message in our database

--- server.js
+++ server.js
@@ -9,19 +9,37 @@ firebase.initializeApp(config);
 const twiliocl = require('twilio')(config.accountSid, config.authToken);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var dbRef = firebase.database().ref('/');
+var db = {};
+
+dbRef.on('value', function (s) {
+	db = s.val();
+});
 
 app.get("/", (req, res) => {
-	res.writeHead(200, {'Content-Type': 'application/json'});
-    firebase.database().ref('/').once('value').then(function (snapshot) {
-        res.end(JSON.stringify(snapshot.val()));
-    });
+    res.end(JSON.stringify(db));
 });
 
 app.post('/sms', (req, res) => {
 	const twiml = new MessagingResponse();
 
-	twiml.message("I cannot provide any guidance, but I will record your report for future input.");
+	twiml.message("Thank you! I cannot provide any guidance right now, but I will record your report for future analysis.");
+
+	const nextNote = db.doctors[0].patients[0].notes.length;
+	firebase.database().ref(`doctors/0/patients/0/notes/${nextNote}`).set({
+		creator: {
+			name: "Sam Craig",
+			phone: "7654644408",
+			role: "Other"
+		},
+		body: req.body.Body,
+		date: new Date(Date.now()).toISOString()
+	})
 
 	res.writeHead(200, { "Content-Type": "text/xml"});
 	res.end(twiml.toString());
