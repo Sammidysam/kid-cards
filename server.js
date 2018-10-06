53258294b932bcadea17fcd93a563f5fcc593185
Sam Craig <craigsamm@gmail.com>
2018-10-06 02:25:41 -0400

    Return our whole database

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ firebase.initializeApp(config);
 
 http.createServer((req, res) => {
 	res.writeHead(200, {'Content-Type': 'application/json'});
-	firebase.database().ref('/users/sam').once('value').then(function (snapshot) {
+	firebase.database().ref('/').once('value').then(function (snapshot) {
 		res.end(JSON.stringify(snapshot.val()));
 	});
 }).listen(1337, '127.0.0.1');
