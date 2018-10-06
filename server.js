f3e593ffd883c52e38a23819108303a3fbbaf2c5
Sam Craig <craigsamm@gmail.com>
2018-10-06 12:55:39 -0400

    Seed database if empty

--- server.js
+++ server.js
@@ -18,9 +18,41 @@ import analysis from "./analysis"
 var dbRef = firebase.database().ref('/');
 var db = {};
 
+const exPatient = {
+	mainphone: "7654644408",
+	name: "Susan Doe"
+}
+
 dbRef.on('value', function (s) {
 	db = s.val();
+
+	if (db !== null && db.doctors !== undefined && db.doctors[0].patients === undefined) {
+		const doctors = db.doctors
+		doctors[0].patients = {
+			0: exPatient
+		}
+		dbRef.set({
+			doctors: doctors
+		})
+	}
+
+	if (db === null || db.doctors === undefined) {
+		dbRef.set({
+			doctors: {
+				0: {
+					email: "john@doe.com",
+					name: "John Doe",
+					patients: {
+						0: exPatient
+					}
+				}
+			}
+		})
+	}
 });
+
+var currentDoctor = 0
+var currentPatient = 0
 
 app.get("/", (req, res) => {
 	res.header('Access-Control-Allow-Origin', '*');
@@ -32,8 +64,8 @@ app.post('/sms', (req, res) => {
 
 	twiml.message(analysis.analyze(db.doctors[0].patients[0], req.body.Body));
 
-	const nextNote = db.doctors[0].patients[0].notes ? db.doctors[0].patients[0].notes.length : 0;
-	firebase.database().ref(`doctors/0/patients/0/notes/${nextNote}`).set({
+	const nextNote = db.doctors[currentDoctor].patients[currentPatient].notes ? db.doctors[currentDoctor].patients[currentPatient].notes.length : 0;
+	firebase.database().ref(`doctors/${currentDoctor}/patients/${currentPatient}/notes/${nextNote}`).set({
 		creator: {
 			name: "Billy Doel",
 			phone: req.body.From,
@@ -42,7 +74,7 @@ app.post('/sms', (req, res) => {
 		body: req.body.Body,
 		date: new Date(Date.now()).toISOString()
 	})
-	db.doctors[0].patients[0].notes[nextNote] = {
+	db.doctors[currentDoctor].patients[currentPatient].notes[nextNote] = {
         creator: {
             name: "Billy Doel",
             phone: req.body.From,
