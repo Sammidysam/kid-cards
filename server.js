1e8efbe1aab654acd14ffd06de8e0a56052977bd
Sam Craig <craigsamm@gmail.com>
2018-10-06 02:14:45 -0400

    Example retrieval of data

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const firebase = require("firebase");
 firebase.initializeApp(config);
 
 http.createServer((req, res) => {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
+	res.writeHead(200, {'Content-Type': 'application/json'});
+	firebase.database().ref('/users/sam').once('value').then(function (snapshot) {
+		res.end(JSON.stringify(snapshot.val()));
+	});
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
